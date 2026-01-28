81426cef965707dff0d67c78f0816f3548bfefc6
DLh0510 <164997585+DLh0510@users.noreply.github.com>
2026-01-28 20:59:39 +0800

    feat: 云计算Workshop项目实训平台

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
 """应用配置"""
 
 CONFIG = {
-    "title": "云计算运维实训平台",
+    "title": "云计算Workshop项目实训平台",
     "subtitle": "",
-    "window_size": "450x580",
-    "logo_size": (80, 80),
+    "window_size": "420x520",
+    "logo_size": (70, 70),
     "logo_file": "yjgj_foot_logo.png",
-    "teacher_url": "http://cvt41-1899555523.cn-north-1.elb.amazonaws.com.cn:81/",
-    "student_url": "http://cvt41-1899555523.cn-north-1.elb.amazonaws.com.cn:82/",
+    "teacher_url": "http://cvt42-654509355.cn-north-1.elb.amazonaws.com.cn:81/",
+    "student_url": "http://cvt42-654509355.cn-north-1.elb.amazonaws.com.cn:82/",
 }
